d51b2c25bc8be5d8b65a4f71a15fe13c732e9d4b
SergeyK <it-dgstars@mail.ru>
2023-03-20 10:16:04 +0300

    Создали файл README.md

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите желаемое количество слов: ");
+int length = Convert.ToInt32(Console.ReadLine());
+string[] newArray = new string[length];
+int num = 0; int str = 3;
+
